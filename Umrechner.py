59ccfa288fd0740616b366ff123c0f4e9999b3c7
iqnnis.уальная <150165739+iqnnis2hacker@users.noreply.github.com>
2024-04-09 17:16:43 +0200

    Update Umrechner.py
    Changed the operating language to english

--- Umrechner.py
+++ Umrechner.py
@@ -1,23 +1,23 @@
 import math
 
 
-def auswahl():
-  print("Bitte wählen Sie eine Option:")
-  print("1: km/h in m/s konvertieren")
-  print("2: m/s in km/h konvertieren")
-  wahl = input("Auswahl: ")
+def selection():
+  print("Please choose an option:")
+  print("1: km/h in m/s")
+  print("2: m/s in km/h)
+  choose = input("Selection: ")
 
-  if wahl == "1":
-    v1 = float(input("Geschwindigkeit in km/h: "))
-    result = v1 * 10 / 36  # Umrechnung km/h in m/s
-    print(f"{v1} km/h sind {result} m/s.")
-  elif wahl == "2":
-    v2 = float(input("Geschwindigkeit in m/s: "))
-    result = v2 * (3600 / 1000)  # Umrechnung m/s in km/h
-    print(f"{v2} m/s sind {result} km/h.")
+  if choose == "1":
+    v1 = float(input("Speed in km/h: "))
+    result = v1 * 10 / 36  
+    print(f"{v1} km/h are {result} m/s.")
+  elif choose == "2":
+    v2 = float(input("Speed in m/s: "))
+    result = v2 * (3600 / 1000)  
+    print(f"{v2} m/s are {result} km/h.")
   else:
-    print("Ungültige Auswahl.")
+    print("Invalid selection.")
 
-auswahl()
+selection()
 
 input("")
